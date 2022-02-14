e82fe378716db6da71ba12704428935e2281a73e
Tomas Drtina <tm.drtina@gmail.com>
2022-02-14 02:17:07 +0100

    Migrate Wrapper to styled component.

--- client/src/components/Wrapper/Wrapper.tsx
+++ client/src/components/Wrapper/Wrapper.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import { Box, Theme } from '@mui/material';
+import { Box, styled } from '@mui/material';
 
-import makeStyles from '@mui/styles/makeStyles';
-import createStyles from '@mui/styles/createStyles';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    wrapper: {
-      maxWidth: theme.breakpoints.values.lg - 40,
-      margin: '0 1rem',
-      [theme.breakpoints.up('lg')]: {
-        margin: '0 auto',
-      },
-    },
-  })
-);
+const StyledBox = styled(Box)(({ theme }) => ({
+  maxWidth: theme.breakpoints.values.lg - 40,
+  margin: '0 1rem',
+  [theme.breakpoints.up('lg')]: {
+    margin: '0 auto',
+  },
+}));
 
 interface IWrapper {
   children: React.ReactNode;
 }
 
-const Wrapper = ({ children }: IWrapper) => {
-  const { wrapper } = useStyles();
-
-  return <Box className={wrapper}>{children}</Box>;
-};
+const Wrapper = ({ children }: IWrapper) => (
+  <StyledBox>{children}</StyledBox>
+);
 
 export default Wrapper;
